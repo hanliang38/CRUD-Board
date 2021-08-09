c91b5735430737cc29e7d34e2bb8c1ec45e36046
root <root@DESKTOP-IS6KDI5.localdomain>
2021-08-09 20:05:43 +0900

    board all data get service

---
+++ src/boards/board.model.ts
@@ -0,0 +1,14 @@
+import internal from 'stream';
+
+export interface Board {
+    id: string;
+    title: string;
+    description: string;
+    like: number;
+    status: BoardStatus;
+}
+
+export enum BoardStatus {
+    PUBLIC = 'PUBLIC',
+    PRIVATE = 'PRIVATE'
+}
