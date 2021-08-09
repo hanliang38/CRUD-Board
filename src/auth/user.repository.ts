954d229e7748359e0b2252a8263666aa185d2099
root <root@DESKTOP-IS6KDI5.localdomain>
2021-08-10 06:04:01 +0900

    password salt + hashing

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,12 +2,19 @@ import { ConflictException, InternalServerErrorException } from '@nestjs/common'
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+
         const { username, password, email } = authCredentialsDto;
-        const user = this.create({ email, username, password });
+
+        // 패스워드 암호화 단방향+salt
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = this.create({ email, username, password: hashedPassword });
         
         try {
             await this.save(user);
