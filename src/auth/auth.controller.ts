f26a38d7e5b864e618d1452c5ed74390be0acd75
root <root@DESKTOP-IS6KDI5.localdomain>
2021-08-10 06:38:28 +0900

    Login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,8 +7,13 @@ export class AuthController {
     constructor(private authService: AuthService) { }
 
 
-@Post('/signup')
-signUp(@Body(ValidationPipe) authcredentialsDto: AuthCredentialsDto): Promise <void> {
-    return this.authService.signUp(authcredentialsDto);
-        }
+    @Post('/signup')
+    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise <void> {
+    return this.authService.signUp(authCredentialsDto);
+    }
+    
+    @Post('/signin')
+    signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+        return this.authService.signIn(authCredentialsDto)
+    }
 }
