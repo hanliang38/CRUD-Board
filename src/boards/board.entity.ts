0ece7a835c6ace7ec4e6a7f03b630a2192bf1c04
root <root@DESKTOP-IS6KDI5.localdomain>
2021-08-10 17:50:19 +0900

    db 1:N

--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/auth/user.entity';
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { BoardStatus } from './board-status.enum';
 
 @Entity()
@@ -16,8 +17,8 @@ export class Board extends BaseEntity {
     status: BoardStatus
 
     // @Column()
-    // like: number
-
-    // @Column()
     // createdAt: Date
+
+    @ManyToOne(type => User, user => user.boards, {eager: false})
+    user: User
 }
